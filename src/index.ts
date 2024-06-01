56e15ddfd9f5b47df2375ea4f1f6f45ebd62ad21
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-06-01 15:16:13 +0100

    chore: Update gitignore and nodemon configuration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import express from 'express';
+import express, { type Express } from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const app = express();
+const app: Express = express();
 
-const PORT = process.env.PORT || 5500;
+const PORT = process.env['PORT'] || 5500;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
